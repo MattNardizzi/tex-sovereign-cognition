c7121b4b8fe42e69614629f8b144d61a32ddd9aa
MattNardizzi <Nardizzi.matt@gmail.com>
2025-05-18 21:54:25 -0400

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+// src/app/page.tsx
+import StrategyCoreShell from "@/components/StrategyCoreShell";
+
+export default function Home() {
+  return (
+    <main className="w-screen h-screen overflow-hidden bg-black">
+      <StrategyCoreShell />
+    </main>
+  );
+}
